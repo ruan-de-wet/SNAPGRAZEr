257825bb5291a9187a42d2dee088d3f98e95c285
Ruan de Wet <rfdewet@gmail.com>
2022-05-18 12:46:38 +0200

    Include Gdays

--- R/calc_SOCeq.R
+++ R/calc_SOCeq.R
@@ -5,10 +5,11 @@
 #' @param DDSOCt Output of calc_DDSOCt()
 #' @param SAND Sand % in top 30 cm soil
 #' @param RAIN MAP for year t (mm/year)
+#' @param Gdays Total number of days in the growing season. Default = 153 (October to March-ish).
 #' @param lowSOC Default = FALSE. Different regression equation for respiration rate is applied for low and high SOC to avoid a negative respiration rate (which isn't physically possible). Threshold for what qualifies as "low SOC" is 4,600 gC/m^2 (i.e. 46 t/ha). Low SOC regression equation is applicable for higher SOC, but just with slightly lower R-squared.
 #' @export
 
-calc_SOCeq = function(PDSOCt, DDSOCt, SAND, RAIN, lowSOC = FALSE) {
+calc_SOCeq = function(PDSOCt, DDSOCt, SAND, RAIN, Gdays, lowSOC = FALSE) {
 
   WETDAYS = (0.00044*RAIN-0.025)*Gdays
 
